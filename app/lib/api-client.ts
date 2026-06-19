ab8e09d0cdbf1ab8936b00dcde0d9d2de739b79b
Kawahara <My.favorite.is.energy@gmail.com>
2026-06-19 12:11:36 +0900

    fix: GETメソッドにContent-Type: application/jsonをつけないようにした

--- app/lib/api-client.ts
+++ app/lib/api-client.ts
@@ -1,11 +1,7 @@
 import { env } from "~/config/env";
 
 async function request<T>(path: string, init?: RequestInit): Promise<T> {
-  const res = await fetch(`${env.backendBaseUrl}${path}`, {
-    headers: { "Content-Type": "application/json" },
-    ...init,
-  });
-
+  const res = await fetch(`${env.backendBaseUrl}${path}`, init);
   if (!res.ok) {
     throw new Response(res.statusText, { status: res.status });
   }
@@ -16,5 +12,9 @@ async function request<T>(path: string, init?: RequestInit): Promise<T> {
 export const apiClient = {
   get: <T>(path: string) => request<T>(path),
   post: <T>(path: string, body: unknown) =>
-    request<T>(path, { method: "POST", body: JSON.stringify(body) }),
+    request<T>(path, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    }),
 };
