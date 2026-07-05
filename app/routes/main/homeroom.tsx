b4c12b4f550f469b055647ac4d75e900c474483d
takarea <157473679+takarea@users.noreply.github.com>
2026-07-06 01:49:06 +0900

    delete:ダミーデータを削除

--- app/routes/main/homeroom.tsx
+++ app/routes/main/homeroom.tsx
@@ -1,6 +1,5 @@
 import { useLoaderData } from "react-router";
 import { HomeRoomPage } from "~/features/homeroom/pages/HomeRoomPage";
-// import type { HomeRoomData } from "~/features/homeroom/model/homeroom";
 import { getHomeRoomData } from "~/features/homeroom/model/homeroom-data";
 
 export async function loader() {
@@ -23,25 +22,5 @@ export default function HomeroomRoute() {
   const Homerooms = useLoaderData<typeof loader>();
   return <HomeRoomPage homerooms={Homerooms} />;
 }
-
-// ダミーデータ
-
-// const Homeroom: HomeRoomData[] = [
-//   {
-//     HomeRoomId: 1,
-//     HomeRoomCode: "11A",
-//     HomeRoomName: "1年Aクラス",
-//   },
-//   {
-//     HomeRoomId: 2,
-//     HomeRoomCode: "11B",
-//     HomeRoomName: "1年Bクラス",
-//   },
-//   {
-//     HomeRoomId: 3,
-//     HomeRoomCode: "12A",
-//     HomeRoomName: "2年Aクラス",
-//   },
-// ];
 
 // クラス名 SQL likeを使用して絞り込みを実装する
