736bea556b82f6c8fbc610f2f12fadf5f98c8d53
haya9830 <newholland33532@gmail.com>
2026-07-06 23:07:22 +0900

    SportsRouteで競技データを表示するための競技リストを追加し、コメントを削除

--- app/routes/main/sports.tsx
+++ app/routes/main/sports.tsx
@@ -6,7 +6,44 @@ export function meta() {
 }
 
 export default function SportsRoute() {
-  //   return <CompetitionListPage competitions={competitions} />; 書き換える
-
-  return <CompetitionListPage competitions={[] as CompetitionData[]} />;
+  return <CompetitionListPage competitions={competitions} />;
 }
+
+const competitions: CompetitionData[] = [
+  {
+    CompetitionId: 2,
+    CompetitionName: "綱引き",
+    Venue: "グラウンド A",
+    StartTime: "09:45",
+  },
+  {
+    CompetitionId: 3,
+    CompetitionName: "玉入れ",
+    Venue: "グラウンド B",
+    StartTime: "10:30",
+  },
+  {
+    CompetitionId: 5,
+    CompetitionName: "借り物競走",
+    Venue: "グラウンド A",
+    StartTime: "12:15",
+  },
+  {
+    CompetitionId: 6,
+    CompetitionName: "リレー",
+    Venue: "トラック",
+    StartTime: "13:00",
+  },
+  {
+    CompetitionId: 7,
+    CompetitionName: "障害物競走",
+    Venue: "グラウンド B",
+    StartTime: "14:00",
+  },
+  {
+    CompetitionId: 8,
+    CompetitionName: "大縄跳び",
+    Venue: "グラウンド A",
+    StartTime: "14:45",
+  },
+];
