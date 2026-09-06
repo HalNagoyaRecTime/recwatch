56505482824566a79479081e4fc5e25126a2f80b
subdayo <Yukidayo1739@gmail.com>
2026-09-06 18:28:22 +0900

    refactor: Vitestの広域変更トリガーを削除する

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,12 +6,5 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     setupFiles: ["./app/test/setup.ts"],
-    forceRerunTriggers: [
-      "**/package.json",
-      "**/package-lock.json",
-      "**/vitest.config.*",
-      "**/vite.config.*",
-      "**/tsconfig*.json",
-    ],
   },
 });
